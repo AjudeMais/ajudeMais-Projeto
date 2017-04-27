dc23b955cba8b6bd8ea204a8725c6199a077bce0
Franck Aragão <fjunior.aragao@gmail.com>
2017-04-26 21:42:29 -0300

    main search

---
+++ Sistema/002-Implementação/AjudeMais-mobile/app/src/doador/java/br/edu/ifpb/ajudemais/activities/MainSearchActivity.java
@@ -0,0 +1,17 @@
+package br.edu.ifpb.ajudemais.activities;
+
+import android.os.Bundle;
+
+import br.edu.ifpb.ajudemais.R;
+
+/**
+ * By Franck
+ */
+public class MainSearchActivity extends AbstractActivity {
+
+    @Override
+    protected void onCreate(Bundlegit  savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_search);
+    }
+}
