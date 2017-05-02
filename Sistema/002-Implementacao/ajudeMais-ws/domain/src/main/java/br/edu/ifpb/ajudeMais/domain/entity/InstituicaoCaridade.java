1ce1f5f9f56e6b6f3f13b8f338309af84e7fbafc
Franck Aragão <fjunior.aragao@gmail.com>
2017-05-02 19:22:05 -0300

    fix #2 issue

--- Sistema/002-Implementacao/ajudeMais-ws/domain/src/main/java/br/edu/ifpb/ajudeMais/domain/entity/InstituicaoCaridade.java
+++ Sistema/002-Implementacao/ajudeMais-ws/domain/src/main/java/br/edu/ifpb/ajudeMais/domain/entity/InstituicaoCaridade.java
@@ -1,6 +1,7 @@
 package br.edu.ifpb.ajudeMais.domain.entity;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -37,6 +38,7 @@ public class InstituicaoCaridade {
 	
 	@NotNull
 	@NotBlank(message = "A descrição deve ser informado")
+	@Column(columnDefinition="TEXT")
 	private String descricao;
 
 	private String telefone;
