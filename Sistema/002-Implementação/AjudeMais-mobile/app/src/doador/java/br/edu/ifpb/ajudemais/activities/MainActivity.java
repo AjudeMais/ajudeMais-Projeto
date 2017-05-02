b85b6fe2cd7b345a9bc70273666f5ee0f7bb01b0
Rafael Feitosa <jose.rafael.feitosa@gmail.com>
2017-05-01 22:20:50 -0300

    a utilização do gps para pegar no inicio a localização atual do doador e exibir campanhas e instituições de acordo com sua localidade

--- Sistema/002-Implementação/AjudeMais-mobile/app/src/doador/java/br/edu/ifpb/ajudemais/activities/MainActivity.java
+++ Sistema/002-Implementação/AjudeMais-mobile/app/src/doador/java/br/edu/ifpb/ajudemais/activities/MainActivity.java
@@ -1,29 +1,49 @@
 package br.edu.ifpb.ajudemais.activities;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 import br.edu.ifpb.ajudemais.R;
 import br.edu.ifpb.ajudemais.TabFragmentMain;
+import me.drakeet.materialdialog.MaterialDialog;
 
-public class MainActivity extends AbstractActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AbstractActivity implements NavigationView.OnNavigationItemSelectedListener, LocationListener  {
 
     private FragmentManager mFragmentManager;
     private FragmentTransaction mFragmentTransaction;
     private FloatingActionButton fab;
+    private MaterialDialog mMaterialDialog;
+    private static final int REQUEST_PERMISSIONS_CODE = 128;
 
 
+    /**
+     *
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        callAccessLocation(findViewById(android.R.id.content));
 
         findViewById(R.id.loadingPanel).setVisibility(View.VISIBLE);
         findViewById(R.id.containerView).setVisibility(View.GONE);
@@ -51,6 +71,158 @@ public class MainActivity extends AbstractActivity implements NavigationView.OnN
         new LoadingCampanhasDoacoesTask().execute();
     }
 
+    /**
+     *
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_PERMISSIONS_CODE:
+                for (int i = 0; i < permissions.length; i++) {
+
+                    if (permissions[i].equalsIgnoreCase(Manifest.permission.ACCESS_FINE_LOCATION)
+                            && grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+
+                        readMyCurrentCoordinates();
+                    }
+
+                }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    /**
+     *
+     * @param message
+     * @param permissions
+     */
+    private void callDialog(String message, final String[] permissions) {
+        mMaterialDialog = new MaterialDialog(this)
+                .setTitle("Permissão")
+                .setMessage(message)
+                .setPositiveButton("Ok", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        ActivityCompat.requestPermissions(MainActivity.this, permissions, REQUEST_PERMISSIONS_CODE);
+                        mMaterialDialog.dismiss();
+                    }
+                })
+                .setNegativeButton("Cancel", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mMaterialDialog.dismiss();
+                    }
+                });
+        mMaterialDialog.show();
+
+    }
+
+    /**
+     *
+     * @param view
+     */
+    public void callAccessLocation(View view) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                callDialog(getString(R.string.msgPermitionUsingGPs), new String[]{Manifest.permission.ACCESS_FINE_LOCATION});
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_PERMISSIONS_CODE);
+            }
+        } else {
+            readMyCurrentCoordinates();
+        }
+    }
+
+    /**
+     *
+     */
+    private void callDialogTurnOnGps() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(getString(R.string.msgPermitionTurnOnGPs))
+                .setCancelable(false)
+                .setPositiveButton(getString(R.string.yes), new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivity(intent);
+                    }
+                })
+                .setNegativeButton(getString(R.string.no), new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    /**
+     * Busca minha localização atual utilizandoo GPS.
+     */
+    private void readMyCurrentCoordinates() {
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        boolean isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        boolean isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        Location location = null;
+        double latitude = 0;
+        double longitude = 0;
+
+        if (!isGPSEnabled) {
+            callDialogTurnOnGps();
+
+        } else {
+
+            if (isNetworkEnabled) {
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 2000, 0, (LocationListener) this);
+                location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                if (location != null) {
+                    latitude = location.getLatitude();
+                    longitude = location.getLongitude();
+                }
+            }
+
+            if (isGPSEnabled) {
+                if (location == null) {
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 0, (LocationListener) this);
+                    location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    if (location != null) {
+                        latitude = location.getLatitude();
+                        longitude = location.getLongitude();
+                    }
+                }
+            }
+
+            Log.i("LOCATION","lat:"+latitude+" :log:"+longitude);
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    /**
+     *
+     */
     private class LoadingCampanhasDoacoesTask extends AsyncTask<Void, Void, String> {
 
         @Override
