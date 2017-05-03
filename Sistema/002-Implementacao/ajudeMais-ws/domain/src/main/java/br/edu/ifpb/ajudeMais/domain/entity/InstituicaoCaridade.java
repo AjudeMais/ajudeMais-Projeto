242582ef71fa796131f36edfdf2a5696d2bee71a
Franck Aragão <fjunior.aragao@gmail.com>
2017-05-03 19:17:25 -0300

    integração de itens doaveis com insituição

--- Sistema/002-Implementacao/ajudeMais-ws/domain/src/main/java/br/edu/ifpb/ajudeMais/domain/entity/InstituicaoCaridade.java
+++ Sistema/002-Implementacao/ajudeMais-ws/domain/src/main/java/br/edu/ifpb/ajudeMais/domain/entity/InstituicaoCaridade.java
@@ -1,5 +1,7 @@
 package br.edu.ifpb.ajudeMais.domain.entity;
 
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -8,6 +10,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
@@ -54,6 +57,12 @@ public class InstituicaoCaridade {
 	@NotNull
 	@OneToOne(cascade = CascadeType.MERGE)
 	private Conta conta;
+	
+
+	@OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+	@Column(name = "itens_doaveis")
+	private List<Categoria> itensDoaveis;
+	
 
 	/**
 	 * @return the id
