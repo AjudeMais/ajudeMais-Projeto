9ca25b469c5c68b48200defdd69e4f991a667d4d
Rafael Feitosa <jose.rafael.feitosa@gmail.com>
2017-05-16 11:52:21 -0300

    validação campos no cadastro de doador

--- Sistema/002-Implementacao/AjudeMais-mobile/app/src/main/java/br/edu/ifpb/ajudemais/utils/AndroidUtil.java
+++ Sistema/002-Implementacao/AjudeMais-mobile/app/src/main/java/br/edu/ifpb/ajudemais/utils/AndroidUtil.java
@@ -3,7 +3,16 @@ package br.edu.ifpb.ajudemais.utils;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.support.annotation.NonNull;
 import android.util.Log;
+import android.widget.EditText;
+
+import com.redmadrobot.inputmask.MaskedTextChangedListener;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import br.edu.ifpb.ajudemais.activities.MainActivity;
 
 /**
  * <p>
@@ -41,5 +50,53 @@ public class AndroidUtil {
         }
         return connected;
     }
+
+    /**
+     * Coloca no edit text uma máscara para celular.
+     * @param editText
+     */
+    public void setMaskPhone(EditText editText){
+        final MaskedTextChangedListener listener = new MaskedTextChangedListener(
+                "([00]) [00000]-[0000]",
+                true,
+                editText,
+                null,
+                new MaskedTextChangedListener.ValueListener() {
+                    @Override
+                    public void onTextChanged(boolean maskFilled, @NonNull final String extractedValue) {
+                        Log.d(MainActivity.class.getSimpleName(), extractedValue);
+                        Log.d(MainActivity.class.getSimpleName(), String.valueOf(maskFilled));
+                    }
+                }
+        );
+
+        editText.addTextChangedListener(listener);
+        editText.setOnFocusChangeListener(listener);
+    }
+
+    /**
+     * Verifica que o campo contém um e-mail válido
+     * @param email
+     * @return
+     */
+    public boolean isEmailValid(String email)
+    {
+        String regExpn =
+                "^(([\\w-]+\\.)+[\\w-]+|([a-zA-Z]{1}|[\\w-]{2,}))@"
+                        +"((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                        +"[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\."
+                        +"([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                        +"[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
+                        +"([a-zA-Z]+[\\w-]+\\.)+[a-zA-Z]{2,4})$";
+
+        Pattern pattern = Pattern.compile(regExpn, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+
+        if(matcher.matches())
+            return true;
+        else
+            return false;
+    }
+
 
 }
