51c081d35cf1750114f8cf8a2a73b578bb1ad66b
Ana Silva <as.anasilva@outlook.com>
2017-05-18 13:03:42 -0300

    Activities de mensageiros finalizadas

---
+++ Sistema/002-Implementacao/AjudeMais-mobile/app/src/mensageiro/java/br/edu/ifpb/ajudemais/activities/EditMensageiroAccountActivity.java
@@ -0,0 +1,15 @@
+package br.edu.ifpb.ajudemais.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.edu.ifpb.ajudemais.R;
+
+public class EditMensageiroAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_mensageiro_account);
+    }
+}
