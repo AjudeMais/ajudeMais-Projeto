88edb10a286404272fffdd14cafc729810eecfb8
Ana Silva <as.anasilva@outlook.com>
2017-05-30 17:52:06 -0300

    Alterações no domíniio

--- Sistema/002-Implementacao/ajudeMais-ws/domain/src/main/java/br/edu/ifpb/ajudeMais/domain/entity/Entrega.java
+++ Sistema/002-Implementacao/ajudeMais-ws/domain/src/main/java/br/edu/ifpb/ajudeMais/domain/entity/Entrega.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
@@ -49,6 +50,9 @@ public class Entrega implements Serializable {
 	 */
 	@Temporal(TemporalType.DATE)
 	private Date dataEntrega;
+	
+	@ManyToOne
+	private Donatario donatario;
 
 	/**
 	 * @return the id
@@ -90,6 +94,20 @@ public class Entrega implements Serializable {
 	 */
 	public void setDataEntrega(Date dataEntrega) {
 		this.dataEntrega = dataEntrega;
+	}
+	
+	/**
+	 * @return the donatario
+	 */
+	public Donatario getDonatario() {
+		return donatario;
+	}
+
+	/**
+	 * @param donatario the donatario to set
+	 */
+	public void setDonatario(Donatario donatario) {
+		this.donatario = donatario;
 	}
 
 	/* (non-Javadoc)
