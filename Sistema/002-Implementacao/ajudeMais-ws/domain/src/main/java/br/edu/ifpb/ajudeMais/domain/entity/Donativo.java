8316825d1e770f1888659e60bfcdfaec2d5d3b38
Franck Aragão <fjunior.aragao@gmail.com>
2017-06-05 19:46:20 -0300

    testes de unidade referentes a camada de dados de doação

--- Sistema/002-Implementacao/ajudeMais-ws/domain/src/main/java/br/edu/ifpb/ajudeMais/domain/entity/Donativo.java
+++ Sistema/002-Implementacao/ajudeMais-ws/domain/src/main/java/br/edu/ifpb/ajudeMais/domain/entity/Donativo.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -62,6 +63,7 @@ public class Donativo implements Serializable {
 	 * 
 	 */
 	@OneToMany(cascade = CascadeType.ALL)
+	@Column(name = "estados_doacao")
 	private List<EstadoDoacao> estadosDaDoacao;
 	
 	/**
