1e5cc13502ab20142959bf8f132a2f6e73213f72
Rafael Feitosa <jose.rafael.feitosa@gmail.com>
2017-06-15 22:10:41 -0300

    correções teste aceitação

--- Sistema/003-Teste/Implementação TESTES ACEITAÇÃO/AjudeMaisTestesAceitacao/src/test/java/br/edu/ifpb/ajudemais/testeaceitacao/seleniumPageObject/campanha/CampanhaPage.java
+++ Sistema/003-Teste/Implementação TESTES ACEITAÇÃO/AjudeMaisTestesAceitacao/src/test/java/br/edu/ifpb/ajudemais/testeaceitacao/seleniumPageObject/campanha/CampanhaPage.java
@@ -4,17 +4,22 @@ import static com.codeborne.selenide.Condition.appears;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-import java.math.BigDecimal;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import br.edu.ifpb.ajudemais.testeaceitacao.seleniumPageObject.AbstractPage;
+import br.edu.ifpb.ajudemais.testeaceitacao.seleniumPageObject.instituicaoCaridade.CriarInstituicaoCaridadePage;
+import br.edu.ifpb.ajudemais.testeaceitacao.seleniumPageObject.instituicaoCaridade.InstituicaoCaridadePage;
+import br.edu.ifpb.ajudemais.testeaceitacao.seleniumPageObject.login.LoginPage;
 
 public class CampanhaPage extends AbstractPage {
 
 	private static final String USERNAME_INSTIUTICAO = "42199149196";
 	private static final String PASSWORD_INSTIUTICAO = "42199149196";
+	private LoginPage loginPage;
+	private InstituicaoCaridadePage instituicaoCaridadePage;
+
 
 	/**
 	 * 
@@ -22,7 +27,8 @@ public class CampanhaPage extends AbstractPage {
 	 */
 	public CampanhaPage(WebDriver driver) {
 		super(driver);
-		// TODO Auto-generated constructor stub
+		loginPage = new LoginPage(driver);
+		instituicaoCaridadePage = new InstituicaoCaridadePage(driver);
 	}
 
 	/**
@@ -69,11 +75,45 @@ public class CampanhaPage extends AbstractPage {
 		open(getUrlBase() + "/home/campaha");
 
 		fazlogin(USERNAME_INSTIUTICAO, PASSWORD_INSTIUTICAO);
+		
+		try {
+			Thread.sleep(1000);
+
+			boolean houveLoginInvalido = loginPage.houveLoginInvalido("Nome de usuário ou senha inválido");
+
+			if (houveLoginInvalido) {
+				addInstituicao();
+
+				Thread.sleep(1000l);
+
+				$(By.xpath("//*[@id=\"content-wrapper\"]/div/div[1]/div/div/div[1]/a")).click();
+				$(By.xpath("//*[@id=\"content-wrapper\"]/div/div[1]/div/div/div[1]/ul/div/ul/a[2]")).click();
+
+				fazlogin(USERNAME_INSTIUTICAO, PASSWORD_INSTIUTICAO);
+			}
+
+			$(By.xpath("//*[@id=\"sidebar-wrapper\"]/ul/li[6]/a")).click();
+
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 
 		$(By.xpath("//*[@id=\"sidebar-wrapper\"]/ul/li[4]/a")).click();
 
 	}
 
+	/**
+	 * Adiciona uma instituição de caridade caso ela não exista.
+	 */
+	private void addInstituicao() {
+		instituicaoCaridadePage.visita();
+
+		CriarInstituicaoCaridadePage createInstituicaoCaridadePage = instituicaoCaridadePage.novo();
+
+		createInstituicaoCaridadePage.addOrEditInstituicaoCaridade("INSTIUTIÇÂO P TESTE", "CRIADA EM CATEGORIA PAGE",
+				USERNAME_INSTIUTICAO, "(83) 99812-2196", "testecat123@teste.com", "58500-000", "Rua Teste", "123",
+				"Centro", "casa");
+	}
 
 	/**
 	 * Clicar no botão novo item e acessa a página de crição/edição de campanha.
