f75c0592f5eb7e2878d37a8b541caa1dd2e30752
Ana Silva <as.anasilva@outlook.com>
2017-07-05 15:18:56 -0300

    Tela de detalhes em progresso

--- Sistema/002-Implementacao/AjudeMais-mobile/app/src/mensageiro/java/br/edu/ifpb/ajudemais/asycnTasks/LoadingSolicitacoesColetaTask.java
+++ Sistema/002-Implementacao/AjudeMais-mobile/app/src/mensageiro/java/br/edu/ifpb/ajudemais/asycnTasks/LoadingSolicitacoesColetaTask.java
@@ -1,8 +1,20 @@
 package br.edu.ifpb.ajudemais.asycnTasks;
 
+import android.os.AsyncTask;
+
+import br.edu.ifpb.ajudemais.domain.Donativo;
+import br.edu.ifpb.ajudemais.remoteServices.DonativoRemoteService;
+
 /**
  * Created by amsv on 04/07/17.
  */
 
-public class LoadingSolicitacoesColetaTask {
+public class LoadingSolicitacoesColetaTask extends AsyncTask<Void, Void, Donativo> {
+
+    private DonativoRemoteService donativoRemoteService;
+
+    @Override
+    protected Donativo doInBackground(Void... params) {
+        return null;
+    }
 }
