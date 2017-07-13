8c3a37910397a9e9dafbe8274c97017931c975a9
Franck Aragão <fjunior.aragao@gmail.com>
2017-07-13 14:10:17 -0300

    documentos

--- Sistema/002-Implementacao/AjudeMais-mobile/app/src/main/java/br/edu/ifpb/ajudemais/remoteServices/AbstractRemoteService.java
+++ Sistema/002-Implementacao/AjudeMais-mobile/app/src/main/java/br/edu/ifpb/ajudemais/remoteServices/AbstractRemoteService.java
@@ -25,7 +25,7 @@ import br.edu.ifpb.ajudemais.handler.MyResponseErrorHandler;
 public abstract class AbstractRemoteService {
 
 
-    protected static final String API = "http://192.168.0.108:8080";
+    protected static final String API = "http://192.168.237.120:8080";
 
     protected RestTemplate restTemplate;
 
